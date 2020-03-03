5285221ce2c974789937aac4772681e6935a0434
wangyao <3050740551@qq.com>
2020-03-03 16:27:32 +0800

    无

--- src/sql/springjdbc/JDBCTemplate.java
+++ src/sql/springjdbc/JDBCTemplate.java
@@ -6,6 +6,8 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -18,13 +20,13 @@ public class JDBCTemplate {
     public static void main(String[] args) {
         JdbcTemplate t = new JdbcTemplate(Druid.getDataSource());
         String sql = "select * from book;";
-        /*List<book> l = t.query(sql, new RowMapper<book>() {
+        List<Book> l = t.query(sql, new RowMapper<Book>() {
             @Override
-            public book mapRow(ResultSet rs, int i) throws SQLException {
-                return new book(rs.getInt("id"),rs.getString("bname"),rs.getString("author"),rs.getDouble("price"),rs.getDate("publishDate"),rs.getInt("number"));
+            public Book mapRow(ResultSet rs, int i) throws SQLException {
+                return new Book(rs.getInt("id"),rs.getString("bname"),rs.getString("author"),rs.getDouble("price"),rs.getDate("publishDate"),rs.getInt("number"));
             }
-        });*/
-        List<Book> l = t.query(sql, new BeanPropertyRowMapper<Book>(Book.class));
+        });
+        //List<Book> l = t.query(sql, new BeanPropertyRowMapper<Book>(Book.class));
         System.out.println(l);
     }
 
