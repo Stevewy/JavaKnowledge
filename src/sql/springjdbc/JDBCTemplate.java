29b4df745a4ff722684bf8e28d9f258ae18524f9
wangyao <3050740551@qq.com>
2020-04-03 16:45:12 +0800

    无

--- src/sql/springjdbc/JDBCTemplate.java
+++ src/sql/springjdbc/JDBCTemplate.java
@@ -20,13 +20,13 @@ public class JDBCTemplate {
     public static void main(String[] args) {
         JdbcTemplate t = new JdbcTemplate(Druid.getDataSource());
         String sql = "select * from book;";
-        List<Book> l = t.query(sql, new RowMapper<Book>() {
+        List<Book> l = t.query(sql, new RowMapper<Book>() {//手写
             @Override
             public Book mapRow(ResultSet rs, int i) throws SQLException {
                 return new Book(rs.getInt("id"),rs.getString("bname"),rs.getString("author"),rs.getDouble("price"),rs.getDate("publishDate"),rs.getInt("number"));
             }
         });
-        //List<Book> l = t.query(sql, new BeanPropertyRowMapper<Book>(Book.class));
+        //List<Book> l = t.query(sql, new BeanPropertyRowMapper<Book>(Book.class));//javabean
         System.out.println(l);
     }
 
