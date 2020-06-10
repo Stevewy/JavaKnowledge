37ed32c36adefafbf2f375e308b74cf35ab75d84
wangyao <3050740551@qq.com>
2020-06-10 20:23:47 +0800

    并发 有睡眠时间

---
+++ src/concurrency/SleepingTask.java
@@ -0,0 +1,23 @@
+package concurrency;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author WangYao
+ * @date 2020/6/10
+ * @function
+ */
+public class SleepingTask extends LiftOff {
+    public void run(){
+        try {
+            while (countDown-- > 0) {
+                System.out.println(status());
+                TimeUnit.MILLISECONDS.sleep(100);
+            }
+        } catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
