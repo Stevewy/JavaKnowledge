51bd15d7de1dccc7a7643f0f45354a5483043eeb
wangyao <3050740551@qq.com>
2020-06-11 21:47:17 +0800

    运行并发

--- src/concurrency/Run.java
+++ src/concurrency/Run.java
@@ -12,13 +12,13 @@ public class Run {
     public static void main(String[] args) {
         //runnable
         ExecutorService exec = Executors.newCachedThreadPool();   //用时创建
-//        ExecutorService exec = Executors.newFixedThreadPool(5);   //一次创建好
+//        ExecutorService exec = Executors.newFixedThreadPool(10);   //一次创建好
 //        ExecutorService exec = Executors.newSingleThreadExecutor(); //只有一个(有序)
-        for (int i = 0; i < 5; i++) {
+//        for (int i = 0; i < 5; i++) {
 //            new Thread(new LiftOff()).start();
 //            exec.execute(new LiftOff());  //管理异步任务执行,无需显示管理线程生命周期
-            exec.execute(new SleepingTask());
-        }
+//            exec.execute(new SleepingTask());
+//        }
 
         //callable
 //        ArrayList<Future<String>> results = new ArrayList<>();
@@ -33,7 +33,13 @@ public class Run {
 //                exec.shutdown();
 //            }
 
+        //priority   好像体现不明显
+//        for (int i = 0; i < 5; i++)
+//            exec.execute(new SimplePriorities(Thread.MIN_PRIORITY));
+//        exec.execute(new SimplePriorities(Thread.MAX_PRIORITY));
 
 
+
+        exec.shutdown();
     }
 }
