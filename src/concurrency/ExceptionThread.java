8268f8111e172340a901b23a3dbc9e08fde8af13
wangyao <3050740551@qq.com>
2020-06-12 21:51:32 +0800

    线程出现异常怎么办.线程组已经废弃,Executor来解决(java se5),异常捕获器

---
+++ src/concurrency/ExceptionThread.java
@@ -0,0 +1,46 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * @author WangYao
+ * @date 2020/6/12
+ * @function
+ */
+public class ExceptionThread implements Runnable {
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+
+    public static void main(String[] args) {
+//        Thread.setDefaultUncaughtExceptionHandler(new MyUncaughtExceptionHandler()); //默认捕获器
+//        try {
+//            ExecutorService exec = Executors.newCachedThreadPool();
+//            exec.execute(new ExceptionThread());
+//        } catch (Exception e){
+            //捕获失败
+//            e.printStackTrace();
+//        }
+        ExecutorService exec = Executors.newCachedThreadPool(new HandlerThreadFactory());
+        exec.execute(new ExceptionThread());
+    }
+}
+
+class MyUncaughtExceptionHandler implements Thread.UncaughtExceptionHandler{
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("caught " + e);
+    }
+}
+
+class HandlerThreadFactory implements ThreadFactory{//从该工厂出来的都带有捕获器
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(r);
+        t.setUncaughtExceptionHandler(new MyUncaughtExceptionHandler());
+        return t;
+    }
+}
