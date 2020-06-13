fd7124cb88b7cebccf6bfec87029937229af52d9
wangyao <3050740551@qq.com>
2020-06-13 21:32:59 +0800

    产生整数,可关闭

---
+++ src/concurrency/IntChecker.java
@@ -0,0 +1,15 @@
+package concurrency;
+
+/**
+ * @author WangYao
+ * @date 2020/6/13
+ * @function
+ */
+public abstract class IntChecker {
+    private volatile boolean canceled = false; //为了原子性和可视性
+    public abstract int next();
+
+    public void cancel(){canceled = true;}
+
+    public boolean isCanceled(){return canceled;}
+}
