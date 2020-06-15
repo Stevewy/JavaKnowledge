e23047730a44ee28f63bbadce04b0b866103f6d6
wangyao <3050740551@qq.com>
2020-06-15 22:04:48 +0800

    本地存储避免同时访问一个资源

---
+++ src/concurrency/Accessor.java
@@ -0,0 +1,41 @@
+package concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author WangYao
+ * @date 2020/6/15
+ * @function 线程本地存储
+ */
+public class Accessor implements Runnable {
+    private final int id;
+
+    public Accessor(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()){
+            ThreadLocalVariableHolder.increment();
+            System.out.println(this);
+            Thread.yield();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "#" + id + ": " + ThreadLocalVariableHolder.get();
+    }
+
+    public static void main(String[] args) throws Exception {
+        ExecutorService exec = Executors.newCachedThreadPool();
+        for (int i = 0; i < 5; i++) {
+            exec.execute(new Accessor(i));
+        }
+        TimeUnit.SECONDS.sleep(3);
+        exec.shutdown();
+    }
+}
