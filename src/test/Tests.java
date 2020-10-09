88c1b8fde953342f8a8e7c629a02c9e23990fd5c
wangyao <3050740551@qq.com>
2020-10-09 20:32:43 +0800

    json转JavaBean

--- src/test/Tests.java
+++ src/test/Tests.java
@@ -1,39 +1,38 @@
-package testclass;
+package test;
 
 import lombok.SneakyThrows;
 import org.junit.Test;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * @author WangYao
  * @date 2020/1/9
- * @function
+ * @function 解析json串,变成javaBean
  */
 public class Tests {
 
     @SneakyThrows
-    static Object parse(Class<?> c, List<String> s) {
+    static Object parse (Class<?> c, List<String> s) {
         Object o = c.getDeclaredConstructor().newInstance();
         int count = 0;
         Field[] fs = c.getDeclaredFields();
-        for(Field f : fs){
+        for (Field f : fs) {
             f.setAccessible(true);
             Class type = f.getType();
-            if(type == String.class)
+            if (type == String.class)
                 f.set(o, s.get(count++));
-            else if(type == byte.class || type == Byte.class)
+            else if (type == byte.class || type == Byte.class)
                 f.set(o, Byte.parseByte(s.get(count++)));
-            else if(type == short.class || type == Short.class)
+            else if (type == short.class || type == Short.class)
                 f.set(o, Short.parseShort(s.get(count++)));
-            else if(type == int.class || type == Integer.class)
+            else if (type == int.class || type == Integer.class)
                 f.set(o, Integer.parseInt(s.get(count++)));
-            else if(type == double.class || type == Double.class)
+            else if (type == double.class || type == Double.class)
                 f.set(o, Double.parseDouble(s.get(count++)));
-            else if(type == boolean.class || type == Boolean.class)
+            else if (type == boolean.class || type == Boolean.class)
                 f.set(o, Boolean.parseBoolean(s.get(count++)));
         }
         return o;
