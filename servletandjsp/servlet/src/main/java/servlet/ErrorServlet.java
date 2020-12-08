7ee7897b872e5add7d0415d130f460d180928da9
wangyao <3050740551@qq.com>
2020-12-08 22:38:31 +0800

    错误页面

---
+++ servletandjsp/servlet/src/main/java/servlet/ErrorServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author WangYao
+ * @date 2020/10/20
+ * @function 处理异常的servlet
+ */
+public class ErrorServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = resp.getWriter();
+        writer.print("<h1>404</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+}
