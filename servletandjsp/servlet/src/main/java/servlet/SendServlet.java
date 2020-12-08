ca44651c4b82e3ef570bd5a604e516d247229966
wangyao <3050740551@qq.com>
2020-12-08 22:39:22 +0800

    填充参数

---
+++ servletandjsp/servlet/src/main/java/servlet/SendServlet.java
@@ -0,0 +1,27 @@
+package servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author WangYao
+ * @date 2020/10/26
+ * @function
+ */
+public class SendServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext context = this.getServletContext();
+        String username = "王耀";
+        context.setAttribute("username", username);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
