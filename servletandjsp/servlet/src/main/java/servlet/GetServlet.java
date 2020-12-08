32b818e7fd8e5e0a274701573f07fce1f794fdc4
wangyao <3050740551@qq.com>
2020-12-08 22:38:45 +0800

    获取参数

---
+++ servletandjsp/servlet/src/main/java/servlet/GetServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author WangYao
+ * @date 2020/10/26
+ * @function
+ */
+public class GetServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext context = this.getServletContext();
+        String username = (String) context.getAttribute("username");
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().print("name:"+username);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
