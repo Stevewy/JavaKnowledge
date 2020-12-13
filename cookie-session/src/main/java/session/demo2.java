dbb7dc3b353b85dae3751f74613ee218e1601bfa
wangyao <3050740551@qq.com>
2020-12-13 14:24:32 +0800

    读取session

---
+++ cookie-session/src/main/java/session/demo2.java
@@ -0,0 +1,31 @@
+package session;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author WangYao
+ * @date 2020/11/4
+ * @function
+ */
+public class demo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;character=utf-8");
+
+        HttpSession session = req.getSession();
+        String name = (String) session.getAttribute("name");
+        System.out.println(name);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
